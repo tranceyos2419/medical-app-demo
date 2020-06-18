ea7b563bc1122049e37c0ca030734f453255f26f
tranceyos2419 <31304661+tranceyos2419@users.noreply.github.com>
2020-06-18 17:03:10 +0900

    fix

--- lib/data/clinics_data.dart
+++ lib/data/clinics_data.dart
@@ -1,7 +1,7 @@
 import 'package:eir_demo/model/clinic.dart';
 import 'package:eir_demo/model/time_slot.dart';
 
-List<Clinic> CLINIC_DATA = [
+List<Clinic> CLINICS_DATA = [
   Clinic('c1', '内科', 'Clinic 1', '090-9307-5603', '香川高松市', '82QR+93 Takamatsu',
       'https', timeslots01, '2020/06/18', '2020/06/19'),
   Clinic('c2', '皮膚科', 'Clinic 2', '090-9307-5603', '香川高松市', '82QR+93 Takamatsu',
