a0376a229867aaddaac82c1ee7248c329ad49c01
tranceyos2419 <31304661+tranceyos2419@users.noreply.github.com>
2020-06-18 17:08:03 +0900

    build: add APPOINTMENTS_DATA

---
+++ lib/data/appointments_data.dart
@@ -0,0 +1,8 @@
+import 'package:eir_demo/model/appointment.dart';
+
+List<Appointment> APPOINTMENTS_DATA = [
+  Appointment('a1', '2020/06/02', '2020/06/03', 'c1', 'Yoshi', 'Nothing is bad',
+      Insurance.benefit, 't11'),
+  Appointment('a2', '2020/06/02', '2020/06/03', 'c1', 'Tom', 'Nothing is okay',
+      Insurance.benefit, 't21')
+];
